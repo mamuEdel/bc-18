800c97f7ab58489e52036e4a3f7228b2b3322e3d
mamuEdel <edelmamu@gmail.com>
2017-05-09 16:55:28 +0300

    Update books_oop.py

--- books_oop.py
+++ books_oop.py
@@ -1,8 +1,8 @@
-class Book(Object):
+class Book(object):
 	#Book class constructor
-	def _init_(self, chapters, number_pages, title):
+	def __init__(self, chapters, number_pages, title):
 		self.__chapters = chapters
-		sel.__title = title
+		self.__title = title
 		self.__number_pages = number_pages
 	# method to show the number of pages contained in a particular book
 	def getPages(self):
@@ -15,19 +15,19 @@ class Book(Object):
 		return self.__title
 # Child class inheriting from parent class
 class Book_Gerner(Book):
-	def _init_(self, chapters, title, number_pages, gerner):
+	def __init__(self, title, chapters, number_pages, gerner):
 		super().__init__( chapters, title, number_pages)
 		self.__gerner = gerner
 	#method to retun book description containing number of chapters, number of pages, gerner and book title
 	def book_description(self):
-		return "The book entitled" + getTitle + "has" + getChapters + "chapters," + getPages + "number of pages and is of the category" + self.__gerner
+		return "The book entitled " + self.getTitle() + " has " + self.getChapters() + " chapters, " + self.getPages() + " number of pages and is of the category " + self.__gerner
+
 class Main_Charecter(Book_Gerner):
-	def _init_(self, character_Name):
-		self.character_Name = character_Name
+	def __init__(self):
+		self.character_Name = "Mon Petite"
 	def book_description(self):
 		return "The books main character of the book is" + self.character_Name
-#
-b = Book_Gerner ( "12", "Lady in Red", "234", "Thriller")
+b = Book_Gerner( "12", "Lady in Red", "234", "Thriller")
 print(b.book_description())
 c = Main_Charecter()
 c.book_description()
